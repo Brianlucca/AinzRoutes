b33524cecaa2b7426cc2409e472429b089ef2cab
Brian Lucca <brianlucca2002@gmail.com>
2026-04-11 04:59:38 -0300

    refactor: reorganize view imports and update view registry

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,24 @@
 import { useState } from 'react';
 import { MainLayout } from './components/layout/MainLayout';
 import { DashboardView } from './views/DashboardView';
+import { DnsOptimizerView } from './views/DnsOptimizerView';
 import { IpAnalyzerView } from './views/IpAnalyzerView';
+import { Ipv4CalculatorView } from './views/Ipv4CalculatorView';
 import { Ipv6DiagnostickView } from './views/Ipv6DiagnostickView';
 import { MtrVisualizerView } from './views/MtrVisualizerView';
 import { PortScannerView } from './views/PortScannerView';
-import { Ipv4CalculatorView } from './views/Ipv4CalculatorView';
 import { ServicesStatusView } from './views/ServicesStatusView';
 import { TermsView } from './views/TermsView';
 
 const viewRegistry: Record<string, React.FC<any>> = {
   dashboard: DashboardView,
+  services: ServicesStatusView,
   ip: IpAnalyzerView,
+  'dns-optimizer': DnsOptimizerView,
+  ports: PortScannerView,
   network: Ipv6DiagnostickView,
   mtr: MtrVisualizerView,
-  ports: PortScannerView,
   ipv4: Ipv4CalculatorView,
-  services: ServicesStatusView,
   terms: TermsView,
 };
 
